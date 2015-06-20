0b82b19a9a48ae3007aa71909274db77e3e336f0
Dean Liddell <deanliddell@gmail.com>
2015-06-19 23:28:30 -0500

    Updated.

--- NationalClimaticDataCenter/Clock.pl
+++ NationalClimaticDataCenter/Clock.pl
@@ -4,18 +4,18 @@ use strict;
 use warnings;
 use constant { LineFeed => "\n" };
 
-my $RECORD;				# each line read from input stream.
-my $GROUP_1;			# regexp capture group.
-my $GROUP_2;			# regexp capture group.
+my $RECORD;             # each line read from input stream.
+my $GROUP_1;            # regexp capture group.
+my $GROUP_2;            # regexp capture group.
 
 # Read input stream. Output is STDOUT
 # which is returned as a stream.
 #
 while ($RECORD = <>) {
-	chomp $RECORD; # remove line-feed carriage-return.
-		#
-		# Is the value too short?
-		#
+    chomp $RECORD; # remove line-feed carriage-return.
+        #
+        # Is the value too short?
+        #
     if (length($RECORD) < 4) {
         print "0000", LineFeed;
         #
@@ -37,7 +37,7 @@ while ($RECORD = <>) {
         #
         # Then extract the four-digits and validate them.
         #
-		$GROUP_1 = $1;
+        $GROUP_1 = $1;
         if ($GROUP_1 =~ /^([01][0-9]|2[0-3])[0-5][0-9]/) {
             print $GROUP_1, LineFeed;
         } else {
@@ -52,8 +52,8 @@ while ($RECORD = <>) {
         # 2) extract 'PM' numbers and validate. Time shift (i.e. -12 or +12) 
         # for numbers recorded as 12-hour clock instead of 24-hour clock.
         #
-		$GROUP_1 = $1 . $2;
-		$GROUP_2 = $4;
+        $GROUP_1 = $1 . $2;
+        $GROUP_2 = $4;
         if ($GROUP_2 =~ /AM/) {
             if ($GROUP_1 =~ /^(0[0-9]|1[0-1])[0-5][0-9]/) {
                 print $GROUP_1, LineFeed;
@@ -83,7 +83,7 @@ while ($RECORD = <>) {
         print "0000", LineFeed;
         #
         # And do the same thing as default on
-		# any value for which there is no test.
+        # any value for which there is no test.
         #
     } else {
         print "0000", LineFeed;
